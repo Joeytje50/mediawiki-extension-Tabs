73dc841bf8f7e77e33bfb2b6ad382c710d89259f
Joeytje50 <joeytje50@gmail.com>
2014-01-15 00:43:09 +0100

    General updates
    - Added support for IE7/8
    - Added better handling for self-closing tabs
    - Sorted examples for per-category viewing
    - Changed other minor problems/features

--- ext.tabs.js
+++ ext.tabs.js
@@ -1,4 +1,23 @@
 $(function() {
-	if (document.getElementById(document.location.hash.substr(1))) {} // TODO: #hash -> label -> data-tabpos -> contentdiv
-	// TODO: IE8- support
+	if ($('#tabs-inputform').css('font-family').replace(/["']/g,'') == 'sans-serif' && $('#tabs-inputform').css('margin') == '1px') {
+		$(function() {
+			$('body').addClass('tabs-oldbrowserscript');
+			$('.tabs-label').click(function(e) { $('#'+$(this).attr('for')).click(); e.preventDefault(); return false; });
+			$('.tabs-input').each(function() {
+				if (this.checked) $(this).addClass('checked');
+			}).change(function() {
+				if (!this.checked) return $(this).removeClass('checked');
+				$(this).parent().find('.checked').removeClass('checked');
+				$(this).addClass('checked');
+				$(this).parents('.tabs').toggleClass('tabs').toggleClass('tabs'); // remove and readd class to recalculate styles for its children.
+				// Credit: Fabrício Matté @ http://stackoverflow.com/a/21122724/1256925
+			});
+			moveToTarget(); // this must run after the change event handler has been added, otherwise browsers without :not() support won't change it.
+		});
+	} else if (document.getElementById(decodeURIComponent(document.location.hash.substr(1)))) 
+		$(moveToTarget);
+	
+	function moveToTarget() {
+		// TODO: #hash -> label -> data-tabpos -> contentdiv
+	}
 });
